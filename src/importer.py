ed70a7f572dfcb62c28402cd9b34f8de0fa83297
Colin Basnett <colin@spexigeo.com>
2020-02-12 18:39:48 -0800

    WIP scaling options

--- src/importer.py
+++ src/importer.py
@@ -2,7 +2,7 @@ import bpy
 import bpy_extras
 import bmesh
 from mathutils import Vector, Matrix, Quaternion
-from bpy.props import StringProperty, BoolProperty, FloatProperty
+from bpy.props import StringProperty, BoolProperty, FloatProperty, EnumProperty
 from .reader import RmfReader
 from .rmf import *
 
@@ -16,21 +16,49 @@ class RMF_OT_ImportOperator(bpy.types.Operator, bpy_extras.io_utils.ImportHelper
 
     # ImportHelper mixin class uses this
     filename_ext = ".rmf"
-
-    filter_glob : StringProperty(
+    filter_glob: StringProperty(
         default="*.rmf",
         options={'HIDDEN'},
-        maxlen=255,  # Max internal buffer length, longer would be clighted.
+        maxlen=255,
+    )
+    should_skip_null_faces: BoolProperty(
+        default=True,
+        name='Skip NULL Faces'
     )
 
-    def add_solid(self, solid):
+    units_items = (
+        ('NATIVE', 'Native', 'Native scale (1 unit = 1/16 ft)'),
+        ('METRIC', 'Metric', 'Metric scale (1 unit = 1m)')
+    )
+    units_default = 'METRIC'
+    units_scales = {
+        'NATIVE': 1.0,
+        'METRIC': 0.01904999939
+    }
+    scale = units_scales[units_default]
+
+    def update_units(self, context):
+        self.scale = self.units_scales[self.units]
+
+    units: EnumProperty(
+        items=units_items,
+        name='Units',
+        default=units_default,
+        options={'ANIMATABLE'},
+        update=update_units
+    )
+
+    def add_solid(self, solid: Rmf.Solid):
         mesh_name = 'Solid.000'
         mesh = bpy.data.meshes.new(mesh_name)
         mesh_object = bpy.data.objects.new(mesh_name, mesh)
+        mesh_object.scale = (self.scale, self.scale, self.scale)
 
         bm = bmesh.new()
         vertex_offset = 0
         for f in solid.faces:
+            if self.should_skip_null_faces and f.texture_name == 'NULL':
+                continue
             for vertex in f.vertices:
                 bm.verts.new(tuple(vertex))
             bm.verts.ensure_lookup_table()
@@ -45,7 +73,7 @@ class RMF_OT_ImportOperator(bpy.types.Operator, bpy_extras.io_utils.ImportHelper
 
         return mesh_object
 
-    def get_collection_for_solid(self, solid):
+    def get_collection_for_solid(self, solid: Rmf.Solid):
         if solid.has_clip:
             return bpy.data.collections['Clip']
         elif solid.has_sky:
@@ -55,10 +83,9 @@ class RMF_OT_ImportOperator(bpy.types.Operator, bpy_extras.io_utils.ImportHelper
         else:
             return bpy.context.scene.collection
 
-
     def add_object(self, object):
         if type(object) == Rmf.Solid:
-            solid = self.add_solid(object)
+            self.add_solid(object)
         elif type(object) == Rmf.Entity:
             # TODO: abstract this out somehow
             entity = object
@@ -72,6 +99,9 @@ class RMF_OT_ImportOperator(bpy.types.Operator, bpy_extras.io_utils.ImportHelper
                     bpy.context.scene.collection.objects.link(light_object)
                     pitch, yaw, roll = map(lambda x: float(x), entity['angles'].split())
                     print(pitch, yaw, roll)
+                elif entity.classname == 'env_model':
+                    entity['model']
+                    pass
             else:
                 # TODO: create a collection
                 brush_entities_collection = bpy.data.collections['Brush Entities']
@@ -82,14 +112,14 @@ class RMF_OT_ImportOperator(bpy.types.Operator, bpy_extras.io_utils.ImportHelper
                     object = self.add_solid(solid)
                     entity_collection.objects.link(object)
         elif type(object) == Rmf.Group:
+            # TODO: we could add grouped objects to a collection
             objects = [self.add_object(x) for x in object.objects]
             # bpy.ops.object.select_all(action='DESELECT')
             # for object in objects:
             #    object.select = True
             # bpy.oops.group.create()
 
-    def import_map(self, map):
-        # TODO: create collections!
+    def import_map(self, map: Rmf.World):
         collections_names = 'Trigger', 'Sky', 'Clip', 'Brush Entities'
         for name in collections_names:
             collection = bpy.data.collections.new(name)
@@ -98,7 +128,6 @@ class RMF_OT_ImportOperator(bpy.types.Operator, bpy_extras.io_utils.ImportHelper
             self.add_object(object)
 
     def execute(self, context):
-        # Now add it all in baybee
         rmf = RmfReader().from_file(self.filepath)
         self.import_map(rmf)
         return {'FINISHED'}
